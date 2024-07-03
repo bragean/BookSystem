896eef3b95b94bfbac58845ef7aeb74a57ddea37
bragean-gett <140440637+bragean-gett@users.noreply.github.com>
2024-07-03 12:18:17 -0500

    changing author fields to allow blank

--- book_system/api/models/author_model.py
+++ book_system/api/models/author_model.py
@@ -5,9 +5,9 @@ from django.db.models import CharField
 
 class Author(Base):
     first_name = CharField(max_length=32)
-    second_name = CharField(max_length=32)
-    surname = CharField(max_length=32)
-    second_surname = CharField(max_length=32)
+    second_name = CharField(max_length=32, blank=True)
+    surname = CharField(max_length=32, blank=True)
+    second_surname = CharField(max_length=32, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.surname}"
